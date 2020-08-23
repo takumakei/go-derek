05ef84ef4998849c5cb375ef556c3c42d3a867a5
takumakei <takumakei@gmail.com>
2020-08-23 16:48:53 +0900

    refactor: Process by ProcessGray

--- derek.go
+++ derek.go
@@ -3,7 +3,6 @@ package derek
 
 import (
 	"image"
-	"image/color"
 )
 
 // ProcessGray returns the image with the Derek Bradley's "Adaptive Thresholding using the Integral Image" filter applied.
@@ -65,57 +64,7 @@ func ProcessGray(src *image.Gray, clusterSize int, threshold int) *image.Gray {
 // Process returns the image with the Derek Bradley's "Adaptive Thresholding using the Integral Image" filter applied.
 // threshold must be between [0, 100].
 func Process(src image.Image, clusterSize int, threshold int) image.Image {
-	bounds := src.Bounds()
-	w, h := bounds.Dx(), bounds.Dy()
-	if w <= 0 || h <= 0 {
-		return src
-	}
-
-	gr := color.Gray16Model
-	si := make([]uint, w*h)
-	ii := make([]uint, w*h)
-	sx := bounds.Min.X
-	for x := 0; x < w; x++ {
-		var sum uint
-		sy := bounds.Min.Y
-		for y := 0; y < h; y++ {
-			i := y*w + x
-			pxv := uint(gr.Convert(src.At(sx, sy)).(color.Gray16).Y)
-			si[i] = pxv
-			sum += pxv
-			if x == 0 {
-				ii[i] = sum
-			} else {
-				ii[i] = sum + ii[i-1]
-			}
-			sy++
-		}
-		sx++
-	}
-
-	threshold = min(max(threshold, 0), 100)
-	clusterSize = max(clusterSize, 0) + 1
-	dst := image.NewGray(image.Rect(0, 0, w, h))
-	pix := dst.Pix
-	piw := dst.Stride
-	for x := 0; x < w; x++ {
-		for y := 0; y < h; y++ {
-			x1 := max(x-clusterSize, 0)
-			x2 := min(x+clusterSize, w-1)
-			y1 := max(y-clusterSize, 0)
-			y2 := min(y+clusterSize, h-1)
-			count := (uint(x2) - uint(x1)) * (uint(y2) - uint(y1))
-			y1w := y1 * w
-			y2w := y2 * w
-			sum := ii[x2+y2w] - ii[x1+y2w] - ii[x2+y1w] + ii[x1+y1w]
-			pxv := si[x+y*w]
-			if pxv*count > sum*uint(threshold)/100 {
-				pix[x+y*piw] = 255
-			}
-		}
-	}
-
-	return dst
+	return ProcessGray(NewGray(src), clusterSize, threshold)
 }
 
 func max(a, b int) int {
