ad9355635db0fadd5dde98b8a5243aee08b5c283
takumakei <takumakei@gmail.com>
2020-08-23 17:13:03 +0900

    refactor: Loop Y before X

--- derek.go
+++ derek.go
@@ -18,31 +18,29 @@ func ProcessGray(src *image.Gray, clusterSize int, threshold int) *image.Gray {
 	sw := src.Stride
 
 	ii := make([]uint, w*h)
-	sx := bounds.Min.X
-	for x := 0; x < w; x++ {
+	for y := 0; y < h; y++ {
 		var sum uint
-		sy := bounds.Min.Y
-		for y := 0; y < h; y++ {
-			pxv := si[sx+sy*sw]
-			sum += uint(pxv)
-			i := x + y*w
-			if x == 0 {
+		s := y * sw
+		i := y * w
+		for x := 0; x < w; x++ {
+			sum += uint(si[x+y*sw])
+			if y == 0 {
 				ii[i] = sum
 			} else {
-				ii[i] = sum + ii[i-1]
+				ii[i] = sum + ii[i-w]
 			}
-			sy++
+			s++
+			i++
 		}
-		sx++
 	}
 
 	thresholdu := uint(min(max(threshold, 0), 100))
 	clusterSize = max(clusterSize, 0) + 1
-	dst := image.NewGray(image.Rect(0, 0, w, h))
+	dst := image.NewGray(bounds)
 	pix := dst.Pix
 	piw := dst.Stride
-	for x := 0; x < w; x++ {
-		for y := 0; y < h; y++ {
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
 			x1 := max(x-clusterSize, 0)
 			x2 := min(x+clusterSize, w-1)
 			y1 := max(y-clusterSize, 0)
